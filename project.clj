6a690bb02301665f4f1a26bf2ab670688f384a35
dejanostojic <ostdejan@gmail.com>
2015-09-29 20:19:11 +0200

    deps cleanup

--- project.clj
+++ project.clj
@@ -2,22 +2,20 @@
   :description "Simple shop for food catering"
   :url "http://sitnoseckana.rs"
   :dependencies [ [org.clojure/clojure "1.7.0"]
-                 [compojure "1.3.4"]
+                 [compojure "1.3.4" :exclusions [org.clojure/tools.reader clj-time]]
                  [selmer "0.8.0"]
                  [ring-server "0.4.0"]
                  [ring/ring-anti-forgery "1.0.0"]
                  [ring/ring-defaults "0.1.5"]
                  [mysql/mysql-connector-java "5.1.34"]
-                 [org.clojure/java.jdbc "0.3.6"]                 
-                 ;[lib-noir "0.9.5"]
                  [j18n "1.0.2"]
-                 ;[com.taoensso/tower "3.0.2"]
                  [liberator "0.13"]
                  [cheshire "5.4.0"]
                  [org.clojure/core.cache "0.6.4"]
                  [buddy "0.5.4"]
                  [com.draines/postal "1.11.3"]
-                 [clj-pdf "2.0.6"]]
+                 ;[clj-pdf "2.0.6"]
+                 ]
   :plugins [[lein-ring "0.9.1"]]
   :ring {:handler sitnoseckana.handler/app
          :init sitnoseckana.handler/init
--- src/sitnoseckana/models/db.clj
+++ src/sitnoseckana/models/db.clj
@@ -1,6 +1,5 @@
 (ns sitnoseckana.models.db
-  (:require [clojure.java.jdbc :as sql]
-            [sitnoseckana.app.properties :as props])
+  (:require [sitnoseckana.app.properties :as props])
   (:import java.sql.DriverManager))
 
 ; TODO INSERT THREAD POOLING!!!
@@ -15,9 +14,6 @@
    :db-name (props/get-key "db_name")
    :autoReconnect true
    :port (props/get-key "db_port")})
-
-(defmacro with-db [f & body]
-  `(sql/with-connection ~db (~f ~@body)))
 
 (. Class (forName (db :classname)))
 (def ^:dynamic conn (DriverManager/getConnection (str "jdbc:" (db :subprotocol) "://" (db :host) ":" (db :port) "/" (db :db-name) "?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8") (db :user) (db :password)))
